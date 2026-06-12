ad51d8c08a28a489844a9604632517e49388b6af
rogeliocoding-web <rogeliocoding@gmail.com>
2026-06-11 19:57:19 -0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,8 @@ import bannerImg from "./assets/background_banner.jpg";
 
 const App = () => {
   return (
-    <main>
-      <div className="wrapper">
-        <header>
-          <img src={bannerImg} alt="" />
-          <h1>Find Your Favorite Movies Without The Hassle.</h1>
-        </header>
-        <p>Search</p>
-      </div>
-    </main>
+   
+   <Home />
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Navbar from "./components/Navbar/navbar.jsx";
+import Home from "./pages/Home/Home.jsx";
+
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Navbar />
 
-    <App />
+    <Home />
   </StrictMode>,
 );
+
